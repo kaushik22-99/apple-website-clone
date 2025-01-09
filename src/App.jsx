cf7ecb6635df5c63ac6b89e755d55c05ed0bde96
Kaushik Elangovan <kaushikelangovan65@gmail.com>
2025-01-09 13:44:52 -0500

    Create basic app structure and component scaffolding
    - Set up main section in App.jsx
    - Create Navbar, Hero, and Highlights components
    - Import and add components to main app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import HighLights from './components/Highlights';
+
 const App = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  return ( <main className="bg-black">
+    <Navbar />
+    <Hero />
+    <HighLights />
+  </main>
   )
 }
 
